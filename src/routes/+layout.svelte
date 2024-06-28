fde5850a2d4f3d3fc997a4a7cdb661f083de628b
Karan Anand <thekarananand@gmail.com>
2024-06-28 13:59:48 +0530

    feat: add DarkMode feature + Header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script>
     import "../app.css"
     import "../fonts/inter/inter.css"
+
+    import Header from "../components/Header.svelte"
 </script>
 
+
+<Header/>
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import { Mode } from "../stores/darkMode";
+</script>
+
+<main data-color-scheme="{$Mode}">
+
+    <h1>Main</h1>
+
+    <p>
+        Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+    </p>
+</main>
+
+<style>
+    main {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        min-height: 80vh;
+    }
+</style>
